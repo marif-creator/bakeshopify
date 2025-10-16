c78b925b0ed43060f2ce1eadcc907b1832ce9ad4
m.arif <m.arif@nabatisnack.co.id>
2025-10-16 16:51:20 +0700

    login page

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,7 +1,7 @@
 // middleware/auth.global.ts
 export default defineNuxtRouteMiddleware((to, from) => {
   // Allow unauthenticated access to homepage
-  if (to.path === '/') return
+  if (to.path === '/' || to.path === '/login') return
 
   const session = useAuth()
   if (!session.data.value) {
