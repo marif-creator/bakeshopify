595ea88cfbde008c22958f1c17443d2eae3ab7c4
m.arif <m.arif@nabatisnack.co.id>
2025-10-16 15:48:12 +0700

    add sidebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/ui'],
+  modules: ['@nuxt/image', '@nuxt/ui', '@sidebase/nuxt-auth'],
   vite: {
     plugins: [
       tailwindcss()
