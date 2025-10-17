e4f18ae43347922657c96f118a6f214798c5a3cd
m.arif <m.arif@nabatisnack.co.id>
2025-10-17 09:45:20 +0700

    google provider

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,7 +4,7 @@ import GoogleProvider from '@sidebase/next-auth/providers/google'
 export default NuxtAuthHandler({
   secret: process.env.AUTH_SECRET || 'your-secret-key',
   providers: [
-    GooleProvider.default({
+    GoogleProvider.default({
       name: 'google',
       type: 'oauth',
       clientId: process.env.GOOGLE_CLIENT_ID,
