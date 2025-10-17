c4520ef1cd0f12512f11692a3dbce6a555632809
m.arif <m.arif@nabatisnack.co.id>
2025-10-17 20:08:00 +0700

    add ts expect error

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,16 +4,10 @@ import Google from '@auth/core/providers/google';
 export default NuxtAuthHandler({
   secret: process.env.AUTH_SECRET || 'your-secret-key',
   providers: [
-    Google({
+    // @ts-expect-error
+    Google.default({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code'
-        }
-      }
     }) as any
   ]
 })
