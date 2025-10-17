c383227804e834e1e942b024ece4bf895485a76e
m.arif <m.arif@nabatisnack.co.id>
2025-10-17 09:55:27 +0700

    GoogleProvider

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,14 +1,12 @@
 import { NuxtAuthHandler } from '#auth'
-import GoogleProvider from '@sidebase/next-auth/providers/google'
+import GoogleProvider from "next-auth/providers/google";
 
 export default NuxtAuthHandler({
   secret: process.env.AUTH_SECRET || 'your-secret-key',
   providers: [
-    GoogleProvider.default({
-      name: 'google',
-      type: 'oauth',
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
           prompt: 'consent',
